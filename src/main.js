6e5e5620a3202e938882e8a5576b6cbd855877f9
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-10 14:17:06 +0000

    Moved some components to app.vue

--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
+/**
+ * @author Gagandeep Singh
+ * @email singh.gagandeep3911@gmail.com
+ * @create date 2020-12-10 14:16:38
+ * @modify date 2020-12-10 14:16:38
+ * @desc [description]
+ */
 import { createApp } from "vue";
 
 import App from "./App.vue";
 import BaseBadge from "./components/BaseBadge.vue";
-import BadgeList from "./components/BadgeList.vue";
-import UserInfo from "./components/UserInfo.vue";
 
-const app = createApp(App);
-
-app.component("base-badge", BaseBadge);
-app.component("badge-list", BadgeList);
-app.component("user-info", UserInfo);
-
-app.mount("#app");
+createApp(App)
+  .component("base-badge", BaseBadge)
+  .mount("#app");
