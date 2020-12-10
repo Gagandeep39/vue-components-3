75b02ec190d7e3395f868428df6a087392cc2788
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-10 19:10:28 +0000

    Added scoped slot

---
+++ src/components/CourseGoals.vue
@@ -0,0 +1,20 @@
+<template>
+  <ul>
+    <li v-for="goal in goals" :key="goal">
+      <!-- Allows accessing goal from parent -->
+      <slot :item="goal"></slot>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      goals: ["My name is Gagan", "Come say Hi <3"],
+    };
+  },
+};
+</script>
+
+<style></style>
