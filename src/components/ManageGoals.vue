aae0c012be0f87051019fdef58fccd4fcdb2b015
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-14 05:22:36 +0000

    Added alert using slots

--- src/components/ManageGoals.vue
+++ src/components/ManageGoals.vue
@@ -1,9 +1,38 @@
 <template>
-  <h1>Manage Goals</h1>
+  <div>
+    <h1>Manage Goals</h1>
+    <input ref="goal" />
+    <button @click="setGoal">Set goal</button>
+  </div>
+  <error-alert v-if="dataIsInvalid">
+    <h2>Input is invalid!</h2>
+    <p>Enter few characters</p>
+    <button @click="confirmError">Ok</button>
+  </error-alert>
 </template>
 
 <script>
-export default {};
+import ErrorAlert from "./ErrorAlert.vue";
+export default {
+  components: { ErrorAlert },
+  data() {
+    return {
+      dataIsInvalid: false,
+    };
+  },
+  methods: {
+    setGoal() {
+      const enteredValue = this.$refs.goal.value;
+      if (enteredValue === "") this.dataIsInvalid = true;
+      else {
+        this.dataIsInvalid = false;
+      }
+    },
+    confirmError() {
+      this.dataIsInvalid = false;
+    },
+  },
+};
 </script>
 
 <style></style>
